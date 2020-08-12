8c4bda579915c3e3408a693f9ecbec11943b2b0b
shijo <shijocs07@gmail.com>
2020-08-12 20:34:38 +0530

    retrofit parsing issue fixed

--- app/src/main/java/com/moviedb/movieapp/models/Movie.kt
+++ app/src/main/java/com/moviedb/movieapp/models/Movie.kt
@@ -2,21 +2,20 @@ package com.moviedb.movieapp.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 @Entity
 data class Movie(
-    @SerializedName("popularity") val popularity : Double,
-    @SerializedName("vote_count") val vote_count : Int,
-    @SerializedName("video") val video : Boolean,
-    @SerializedName("poster_path") val poster_path : String,
-    @PrimaryKey(autoGenerate = false) @SerializedName("id") val id : Int,
-    @SerializedName("adult") val adult : Boolean,
-    @SerializedName("backdrop_path") val backdrop_path : String,
-    @SerializedName("original_language") val original_language : String,
-    @SerializedName("original_title") val original_title : String,
-    @SerializedName("title") val title : String,
-    @SerializedName("vote_average") val vote_average : Int,
-    @SerializedName("overview") val overview : String,
-    @SerializedName("release_date") val release_date : String
+    @Expose @SerializedName("popularity") val popularity : Double,
+    @Expose @SerializedName("vote_count") val vote_count : Int,
+    @Expose @SerializedName("poster_path") val poster_path : String,
+    @Expose @PrimaryKey(autoGenerate = false) @SerializedName("id") val id : Int,
+    @Expose @SerializedName("adult") val adult : Boolean,
+    @Expose @SerializedName("backdrop_path") val backdrop_path : String,
+    @Expose @SerializedName("original_language") val original_language : String,
+    @Expose @SerializedName("original_title") val original_title : String,
+    @Expose @SerializedName("title") val title : String,
+    @Expose @SerializedName("overview") val overview : String,
+    @Expose @SerializedName("release_date") val release_date : String
 )
--- app/src/main/java/com/moviedb/movieapp/network/model/MovieResult.kt
+++ app/src/main/java/com/moviedb/movieapp/network/model/MovieResult.kt
@@ -1,12 +1,13 @@
 package com.moviedb.movieapp.network.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.moviedb.movieapp.models.Movie
 
 
 data class MovieResult(
-    @SerializedName("results") val movies : List<Movie>,
-    @SerializedName("page") val page : Int,
-    @SerializedName("total_results") val total_results : Int,
-    @SerializedName("total_pages") val total_pages : Int
+    @Expose @SerializedName("results") val movies : List<Movie>,
+    @Expose @SerializedName("page") val page : Int,
+    @Expose @SerializedName("total_results") val total_results : Int,
+    @Expose @SerializedName("total_pages") val total_pages : Int
 )
